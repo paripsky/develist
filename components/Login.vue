3d109cabdd53e02b419257c17213d5d3f1014ebe
paripsky <paripsky@gmail.com>
2023-06-23 23:38:30 +0300

    fix login on mobile

--- components/Login.vue
+++ components/Login.vue
@@ -1,6 +1,19 @@
 <template>
-  <UButton v-if="!user" @click="login('google')">Login with Google</UButton>
-  <UButton v-if="!user" @click="login('github')">Login with Github</UButton>
+  <div class="hidden sm:flex gap-2" v-if="!user">
+    <UButton @click="login('google')">Login with Google</UButton>
+    <UButton @click="login('github')">Login with Github</UButton>
+  </div>
+  <div class="flex sm:hidden gap-2" v-if="!user">
+    <UButton @click="isOpen = true">Login</UButton>
+  </div>
+  <UModal v-if="!user" v-model="isOpen" @close="resetAndClose">
+    <UCard>
+      <div class="flex flex-col gap-2">
+        <UButton @click="login('google')">Login with Google</UButton>
+        <UButton @click="login('github')">Login with Github</UButton>
+      </div>
+    </UCard>
+  </UModal>
   <UDropdown v-if="user" :items="menuItems" :popper="{ placement: 'bottom-end' }">
     <button class="focus-visible:outline-primary-500">
       <UAvatar :src="user?.user_metadata?.avatar_url" />
@@ -9,6 +22,7 @@
 </template>
 
 <script setup>
+const isOpen = ref(false);
 const user = useSupabaseUser();
 const client = useSupabaseAuthClient();
 const toast = useToast();
